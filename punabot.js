d6f0fd23e56888a7140604d607314a6f1e080b14
Puna Gamer 32 <punagamer32@gmail.com>
2026-02-01 17:14:10 -0600

    Update punabot.js

--- punabot.js
+++ punabot.js
@@ -144,7 +144,6 @@ client.on('messageCreate', async (message) => {
     return message.reply('⚠️ Error fetching party info.');
   }
 }
-  }
   if (message.content.startsWith('!rps')) {
     const opponent = message.mentions.users.first();
     if (!opponent) return message.reply('Mention someone to challenge!');
@@ -189,7 +188,3 @@ setInterval(async () => {
 }, 150000); // every 2.5 minutes
 // --- Login ---
 client.login(DISCORD_TOKEN);
-
-
-
-
